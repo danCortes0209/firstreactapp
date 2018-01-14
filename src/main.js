f2d504051300ef5a87dc5878add74db1826d1ce7
LAPTOP-PLDLUM9D\pingüino n_n <developmentsupport@mantizgs.com>
2018-01-14 02:18:16 -0600

    ready to push to github

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-console.log('Hello World!');
 import React from 'react';
 import ReactDOM from 'react-dom';
  
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-   	"Hello, world!",
+   	"Hello world",
     document.getElementById('mount')
   );
 });
